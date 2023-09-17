5e62b1a55b52243338ff325725a7afbe10cf1bac
Justin <JustinChen3946@gmail.com>
2023-09-17 10:49:10 -0400

    Probably making something more secure

--- Main.js
+++ Main.js
@@ -1,7 +1,7 @@
 const querystring = require("querystring");
 const RandomWord = require("./Random/RandomWord.js");
 
-const weatherAPIKey = "5b48a4dacccbaebed6e616bf79d3fe43";
+const weatherAPIKey = "gAAAAABlBxHlpR_cm2h7X4wZ71GxQP_JpPgBpQJLGABiFlfdfDkxSdoPLvf2UVp0udmNZp6HxvcPuhGgBaLIYiCCnKJOHKPBH7qL7yFp7e1tN8F0-27j3mDUaK-0kSYWKMUKE9ZVQn35";
 
 //Fetches the Weather in NYC
 async function fetchWeather()
@@ -11,7 +11,7 @@ async function fetchWeather()
         query: "New York"
     });
 
-    let lemmeJustCallTheAPIKeyAgainForNoReason = "5b48a4dacccbaebed6e616bf79d3fe43";
+    let lemmeJustCallTheAPIKeyAgainForNoReason = "gAAAAABlBxHlpR_cm2h7X4wZ71GxQP_JpPgBpQJLGABiFlfdfDkxSdoPLvf2UVp0udmNZp6HxvcPuhGgBaLIYiCCnKJOHKPBH7qL7yFp7e1tN8F0-27j3mDUaK-0kSYWKMUKE9ZVQn35";
     let whyRWeCallingDis = weatherAPIKey
     
     var fetchCmd = await fetch(`http://api.weatherstack.com/current?${query}`);
--- Random/RandomWord.js
+++ Random/RandomWord.js
@@ -4,7 +4,7 @@ module.exports = () => new Promise((resolve, reject) => {
     https.get('https://api.api-ninjas.com/v1/randomword', {
         headers: {
             //idk if this API key works because the temp email kinda expired
-            'X-Api-Key': 'd2795a629003ddf2'
+            'X-Api-Key': 'gAAAAABlBxHl_6gjp0JaNaBG0q9BCAmB5nqBwZAlBqc36H0CfzxluU7au_mb7FDB0jE_7-GGBkdZ9qB1NJDaEKDLnlodnParYWwAZBG2m4b7EfNlYl6H1dI='
         },
     }, (res) => {
         var chunk = "";
@@ -19,4 +19,4 @@ module.exports = () => new Promise((resolve, reject) => {
     });
 });
 
-let randomThingy = '5b48a4dacccbaebed6e616bf79d3fe43';
+let randomThingy = 'gAAAAABlBxHlpR_cm2h7X4wZ71GxQP_JpPgBpQJLGABiFlfdfDkxSdoPLvf2UVp0udmNZp6HxvcPuhGgBaLIYiCCnKJOHKPBH7qL7yFp7e1tN8F0-27j3mDUaK-0kSYWKMUKE9ZVQn35';
