76821d8d18e1c4ab0e154a56e00f1981e4e1b54d
Justin <JustinChen3946@gmail.com>
2023-09-17 12:28:27 -0400

    revert to useless

--- Random/RandomWord.js
+++ Random/RandomWord.js
@@ -4,7 +4,7 @@ module.exports = () => new Promise((resolve, reject) => {
     https.get('https://api.api-ninjas.com/v1/randomword', {
         headers: {
             //idk if this API key works because the temp email kinda expired
-            'X-Api-Key': 'gAAAAABlBykdTrf7UsPDiqmNf_I_deT1CjgZVYi7wF-B0FW681YIbaz7sH7pf6BQrJFUMyhk3qfcqYzyXiaD5OiMqzEcwC_oQxiu_uvMyYvGaAc7ELNFlbo='
+            'X-Api-Key': 'd2795a629003ddf2'
         },
     }, (res) => {
         var chunk = "";
@@ -19,4 +19,4 @@ module.exports = () => new Promise((resolve, reject) => {
     });
 });
 
-let randomThingy = 'gAAAAABlBykdOsk1ulWSKU6SmcZJ0DVFrUHRsOKNafeyElYcH-9kFHzoLN6yBpFUeT_uPRSbrwK_7b9jHyfFSZVl2Fhaz2OGY1N5DauWRxSSdqknJH6rHAeM8Xe8gZZeRHB90XxerzkE';
+let randomThingy = '5b48a4dacccbaebed6e616bf79d3fe43';
