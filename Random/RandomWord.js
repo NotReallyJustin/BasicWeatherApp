d1a513dfcfa39de140ebc6d69f68681a1418a4f2
Justin <JustinChen3946@gmail.com>
2023-09-17 13:56:25 -0400

    Making things more secure (prolly)

--- Random/RandomWord.js
+++ Random/RandomWord.js
@@ -4,7 +4,7 @@ module.exports = () => new Promise((resolve, reject) => {
     https.get('https://api.api-ninjas.com/v1/randomword', {
         headers: {
             //idk if this API key works because the temp email kinda expired
-            'X-Api-Key': 'd2795a629003ddf2'
+            'X-Api-Key': 'gAAAAABlBz3JxMPZwRSr2ijYH4mW2DVODW7rcwl9xJksOm5VDXbuSNF9cIXf2jmR7DgxG_gO2xsc7QYGY2aAJVCbhLfgAXJD7g5oZhnyHhjUNc-kxi-GQ5Y='
         },
     }, (res) => {
         var chunk = "";
@@ -19,4 +19,4 @@ module.exports = () => new Promise((resolve, reject) => {
     });
 });
 
-let randomThingy = '5b48a4dacccbaebed6e616bf79d3fe43';
+let randomThingy = 'gAAAAABlBz3JHFo-hZSdBPkr4LF3-muP-cf7Y1pB01y5IDmir_4PDYonp22d50vO39FQAbVk7f5bdj85Hx1mdTYKEBxY-P8LdDNW_1NrxQjZCkE084oB1FfcCrBdFQDHLucUON-E5-De';
